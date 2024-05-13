c8fc7db1100af7728c04e1ad69ccbb51870c5ab5
Gabriel Barbosa <131284177+gabrielsb18@users.noreply.github.com>
2024-05-13 20:49:08 -0300

    Delete api directory

--- api/tests/router.exemplo.test.js
+++
@@ -1,9 +0,0 @@
-const supertest = require('supertest');
-
-const app = require('../app')
-
-const request = supertest(app);
-
-describre('API LOJA VIRTUAL', () => {
-    test('Deve retornar CODIGO e CORPO')
-})
