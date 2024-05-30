f1ca97f11757a96686db4650c4843521125aab62
gabrielsb18 <gbmodelator@gmail.com>
2024-05-29 22:00:13 -0300

    closes #57  Middleware valida Dados concluído

--- routes/router_notes.js
+++ routes/router_notes.js
@@ -2,10 +2,10 @@ const controller = require("../controllers/controllerNotes");
 const express = require("express");
 const router = express.Router();
 
-router.post("/", controller.criar);
+router.post("/", controller.validaDados,controller.criar);
 router.get("/", controller.listarNotes);
-router.get("/:id", controller.buscarPeloID,controller.obterNota);
-router.put("/:id",controller.buscarPeloID,controller.atualizar);
-router.delete("/:id",controller.buscarPeloID,controller.remover);
+router.get("/:id", controller.buscarPeloID, controller.obterNota);
+router.put("/:id", controller.buscarPeloID, controller.validaDados, controller.atualizar);
+router.delete("/:id",controller.buscarPeloID, controller.remover);
 
 module.exports = router;
