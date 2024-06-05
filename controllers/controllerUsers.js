87feb4b3e799b02754d10d8d8d9a250827a28ee0
gabrielsb18 <gbmodelator@gmail.com>
2024-06-05 17:38:34 -0300

    Closes #67 função responsável por cryptografar senha

--- controllers/controllerUsers.js
+++ controllers/controllerUsers.js
@@ -1,14 +1,20 @@
-const crypto = required("crypto");
+const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const Usuario = require("../models/model_users");
+
+function cryptografaSenha(senha, salt) {
+    const hash = crypto.createHmac('sha256', salt);
+    hash.update(senha);
+    return hash.digest('hex');
+}
 
 async function criar(req, res) {
     const {email, senha} = req.body;
     const salt = crypto.randomBytes(16).toString("hex");
     const newUsuario = await Usuario.create({
-        email, senha: cifraSenha(senha, salt), salt
+        email, senha: cryptografaSenha(senha, salt), salt
     });
-    res.Status(201)
+    res.status(201)
     .json({
         id: newUsuario._id.toString(),
         email: newUsuario.email,
