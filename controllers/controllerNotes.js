e47bdfbcd0efa982ba834d57a904f3e39c0dc56a
RBDSVIANA <rainier.273@gmail.com>
2024-06-12 16:10:22 -0300

    Atualização do swagger,yaml

--- controllers/controllerNotes.js
+++ controllers/controllerNotes.js
@@ -1,44 +1,44 @@
 const mongoose = require("mongoose");
 const Notes = require("../models/model_notes");
 
-async function criar(req,res) {
+async function criar(req, res) {
     const nota = await Notes.create(req.body);
     res.status(201).json(nota);
 }
 
-async function validaDados(req, res, next){
-    const nota = new Notes(req.body)
-    try{
+async function validaDados(req, res, next) {
+    const nota = new Notes(req.body);
+    try {
         await nota.validate();
         next();
-    }catch(err){
-        res.status(422).json({msg:"Dados invalidos!"})
-    };
-}
-
-async function listarNotes(req,res){
-    const notas = await Notes.find({}) 
-    res.json(notas)
-}
-
-async function buscarPeloID(req,res, next){
-    try {
-        const id = new mongoose.Types.ObjectId(req.params.id)
-        const nota = await Notes.findOne({_id:id})
-        next()
     } catch (err) {
-        res.status(404).json({msg:"Produto não encontrado"})
+        res.status(422).json({ msg: "Dados invalidos!" });
     }
 }
 
-async function obterNota(req,res, next){
+async function listarNotes(req, res) {
+    const notas = await Notes.find({});
+    res.json(notas);
+}
+
+async function buscarPeloID(req, res, next) {
     try {
-        const id = new mongoose.Types.ObjectId(req.params.id)
-        const nota = await Notes.findOne({_id:id})
-        res.json(nota)
-        next()
+        const id = new mongoose.Types.ObjectId(req.params.id);
+        const nota = await Notes.findOne({ _id: id });
+        next();
     } catch (err) {
-        res.status(404).json({msg:"Produto não encontrado"})
+        res.status(404).json({ msg: "Produto não encontrado" });
+    }
+}
+
+async function obterNota(req, res, next) {
+    try {
+        const id = new mongoose.Types.ObjectId(req.params.id);
+        const nota = await Notes.findOne({ _id: id });
+        res.json(nota);
+        next();
+    } catch (err) {
+        res.status(404).json({ msg: "Produto não encontrado" });
     }
 }
 
@@ -48,10 +48,18 @@ async function remover(req, res) {
     res.status(204).end();
 }
 
-async function atualizar(req,res){
+async function atualizar(req, res) {
     const id = new mongoose.Types.ObjectId(req.params.id);
-    const nota = await Notes.findOneAndUpdate({_id: id },req.body)
+    const nota = await Notes.findOneAndUpdate({ _id: id }, req.body);
     res.json(nota);
-} 
+}
 
-module.exports={criar, listarNotes,buscarPeloID, obterNota,remover, atualizar, validaDados}
+module.exports = {
+    criar,
+    listarNotes,
+    buscarPeloID,
+    obterNota,
+    remover,
+    atualizar,
+    validaDados,
+};
--- routes/router_notes.js
+++ routes/router_notes.js
@@ -2,9 +2,9 @@ const controller = require("../controllers/controllerNotes");
 const express = require("express");
 const router = express.Router();
 
-router.post("/", controller.validaDados,controller.criar);
 router.get("/", controller.listarNotes);
 router.get("/:id", controller.buscarPeloID, controller.obterNota);
+router.post("/", controller.validaDados,controller.criar);
 router.put("/:id", controller.buscarPeloID, controller.validaDados, controller.atualizar);
 router.delete("/:id",controller.buscarPeloID, controller.remover);
 
