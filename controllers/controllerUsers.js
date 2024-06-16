8353f695373884a812356cc5fa76bb92a0669b94
gabrielsb18 <gbmodelator@gmail.com>
2024-06-15 23:48:16 -0300

    validaToken retirado

--- controllers/controllerUsers.js
+++ controllers/controllerUsers.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const Usuario = require("../models/model_users");
@@ -6,16 +7,6 @@ function cryptografaSenha(senha, salt) {
     const hash = crypto.createHmac('sha256', salt);
     hash.update(senha);
     return hash.digest('hex');
-}
-
-function validarToken(req, res, next) {
-    const token = req.headers["authorization"]
-    try {
-        (jwt.verify(token, process.env.SEGREDO))
-        next()
-    } catch (error) {
-        res.status(401).json({msg: "ACESSO NEGADO!"})
-    }
 }
 
 async function criar(req, res) {
@@ -35,16 +26,16 @@ async function criar(req, res) {
 
 async function login(req, res) {
     const usuario = await Usuario.findOne({ email: req.body.email });
-    console.log(usuario.senha, usuario.salt)
+    console.log(usuario.senha, usuario.salt);
     if (usuario.senha === cryptografaSenha(req.body.senha, usuario.salt)) {
         res.json({
             token: jwt.sign({ email: usuario.email }, process.env.SEGREDO, {
-                expiresIn: "1h",
-            }),
+                expiresIn: '1h'
+            })
         });
     } else {
-        res.status(401).status({ msg: "Acesso negado" });
+        res.status(401).json({ msg: "Acesso negado" });
     }
 }
 
-module.exports = {criar, login, validarToken}
+module.exports = {criar, login}
