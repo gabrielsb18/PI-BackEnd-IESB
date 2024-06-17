9624c153b7570eb58fa0ed6bdf0f1d3ed864d599
gabrielsb18 <gbmodelator@gmail.com>
2024-06-17 14:53:09 -0300

    closes #104 teste retornando 500 e ajustes

--- tests/router_users.test.js
+++ tests/router_users.test.js
@@ -1,6 +1,7 @@
 const app = require("../app");
 const supertest = require("supertest");
 const request = supertest(app);
+const Usuario = require("../models/model_users");
 
 let id = null;
 
@@ -15,15 +16,30 @@ describe("API notes-Usuarios", function(){
         })
         id=result.body.id.toString();
         expect(result.status).toBe(201);
-        expect(result.type).toBe("application/json")
-    })
+        expect(result.type).toBe("application/json");
+    });
 
-    test("Deve retornar 400 no POST /users", async () => {
+    test("Deve retornar 400 no POST /users - Erro: Email com espaços em branco", async () => {
         const result = await request.post("/users").send({
             email: "cruzdemalta gmail.com",
             senha: "abcdefgh",
         });
         expect(result.status).toBe(400);
         expect(result.type).toBe("application/json");
+        expect(result.body).toHaveProperty("errors");
     });
-})
+
+    test("Deve retornar 500 em caso de erro no servidor", async ()=>{
+        jest.spyOn(Usuario, 'create').mockImplementation(() => {
+            throw new Errors("Erro interno do servidor");
+        });
+
+        const result = await request.post("/users").send({
+            email: "testeDeErroInterno@gmail.com",
+            senha: "SenhaSegura"
+        });
+        expect(result.status).toBe(500);
+        expect(result.type).toBe("application/json");
+        expect(result.body).toHaveProperty("error");
+    })
+});
