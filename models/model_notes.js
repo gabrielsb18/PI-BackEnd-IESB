b890849f297d32da8172c0e68d6f02e1eea21881
gabrielsb18 <gbmodelator@gmail.com>
2024-11-25 10:22:01 -0300

    Closes #120 implementando atributo de status no schema de notas

--- models/model_notes.js
+++ models/model_notes.js
@@ -14,7 +14,14 @@ const NotesSchema = new mongoose.Schema({
     createdData: {
         type: Date,
         default: Date.now
-    }
+    },
+
+    status: {
+        type: String,
+        enum: ['pendente', 'concluida'],
+        default: 'pendente'
+    },
+
 })
 
 module.exports = mongoose.model("Notes", NotesSchema);
