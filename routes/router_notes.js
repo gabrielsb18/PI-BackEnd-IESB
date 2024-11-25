a0a9d7758e79a2bc2390ab4da48771f86c4995e7
gabrielsb18 <gbmodelator@gmail.com>
2024-11-25 10:51:07 -0300

    Closes #126 Atualizando a função listarNotes para filtrar notas pelo usuário autenticado

--- routes/router_notes.js
+++ routes/router_notes.js
@@ -3,7 +3,7 @@ const middleware = require("../middlewares/validatoken");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", middleware.validaToken,controller.listarNotes);
+router.get("/:userId", middleware.validaToken,controller.listarNotes);
 router.get("/:id", middleware.validaToken, controller.buscarPeloID, controller.obterNota);
 router.post("/", middleware.validaToken, controller.validaDados,controller.criar);
 router.put("/:id", middleware.validaToken, controller.buscarPeloID, controller.validaDados, controller.atualizar);
