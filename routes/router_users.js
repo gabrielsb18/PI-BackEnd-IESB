0b0b4c7789a8c3f5e53244bbf063ce74cccc56d5
gabrielsb18 <gbmodelator@gmail.com>
2024-11-25 11:24:02 -0300

    Closes #118 implementando controller para buscar as informações do usuario logado

--- routes/router_users.js
+++ routes/router_users.js
@@ -1,7 +1,9 @@
 const controller = require("../controllers/controllerUsers");
+const middleware = require("../middlewares/validatoken");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", middleware.validaToken, controller.obterUser);
 router.post("/", controller.criar);
 router.post("/login", controller.login);
 router.post("/renovar", controller.renovaToken)
