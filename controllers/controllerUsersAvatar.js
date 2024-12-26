cf85c367c9d426b5624dc094cf086dece519bcaa
gabrielsb18 <gbmodelator@gmail.com>
2024-12-25 23:20:30 -0300

    feat: Closes #160 convertendo a classe controllerUsersAvatar em funcao

--- controllers/controllerUsersAvatar.js
+++ controllers/controllerUsersAvatar.js
@@ -1,31 +1,26 @@
-const mongoose = require("mongoose");
 const Usuario = require("../models/model_users");
 const DiskStorage = require("../providers/DiskStorage");
 
-class controllerUsersAvatar {
-    async update(req, res) {
-        const userId = req.userId;
-        const avatarFilename = req.file.filename;
+async function updateUserAvatar(req, res) {
+    const userId = req.userId;
+    const avatarFilename = req.file.filename;
 
-        const diskStorage = new DiskStorage();
+    const user = await Usuario.findById(userId);
 
-        const user = await Usuario.findById(userId);
-        
-        if (!user) {
-            return res.status(401).json({ msg: "Somente usuários autenticados podem mudar o avatar" });
-        }
-
-        if(user.avatar){
-            await diskStorage.deleteFile(user.avatar);
-        }
-
-        const filename = await diskStorage.saveFile(avatarFilename);
-        user.avatar = filename;
-
-        await Usuario.findOneAndUpdate({ _id: userId }, { avatar: filename });
-
-        return res.status(201).json({msg: "Foto atualizada com sucesso", user})
+    if (!user) {
+        return res.status(401).json({ msg: "Somente usuários autenticados podem mudar o avatar" });
     }
+
+    if (user.avatar) {
+        await DiskStorage.deleteFile(user.avatar);
+    }
+
+    const filename = await DiskStorage.saveFile(avatarFilename);
+    user.avatar = filename;
+
+    await Usuario.findOneAndUpdate({ _id: userId }, { avatar: filename });
+
+    return res.status(201).json({ msg: "Foto atualizada com sucesso", user })
 }
 
-module.exports = controllerUsersAvatar;
+module.exports = { updateUserAvatar };
