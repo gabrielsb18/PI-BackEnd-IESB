e6e00718e07834c356664394de209cb1ddbb0d3f
gabrielsb18 <gbmodelator@gmail.com>
2024-12-25 23:14:49 -0300

    feat: Closes #159 convertendo a classe diskStorage em funcao

--- providers/DiskStorage.js
+++ providers/DiskStorage.js
@@ -2,29 +2,26 @@ const fs = require('fs');
 const path = require('path');
 const uploadConfig = require("../config/upload")
 
-class DiskStorage {
+async function saveFile(file) {
 
-    async saveFile(file){
+    await fs.promises.rename(
+        path.resolve(uploadConfig.TMP_FOLDER, file),
+        path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    );
 
-        await fs.promises.rename(
-            path.resolve(uploadConfig.TMP_FOLDER, file),
-            path.resolve(uploadConfig.UPLOADS_FOLDER, file)
-        );
-
-        return file;
-    }
-
-    async deleteFile(file){
-       const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
-       
-       try {
-            await fs.promises.stat(filePath);
-       } catch {
-            return;
-       }
-
-       await fs.promises.unlink(filePath);
-    }
+    return file;
 }
 
-module.exports = DiskStorage;
+async function deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
+
+    try {
+        await fs.promises.stat(filePath);
+    } catch {
+        return;
+    }
+
+    await fs.promises.unlink(filePath);
+}
+
+module.exports = { saveFile, deleteFile };
