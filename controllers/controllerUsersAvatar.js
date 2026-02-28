29ebf4c5a50c83c70064c3dbed19fc917c7591be
gabrielsb18 <gbmodelator@gmail.com>
2026-02-27 21:40:11 -0300

    feat(backend): implement showAvatar controller to serve image blob

--- controllers/controllerUsersAvatar.js
+++ controllers/controllerUsersAvatar.js
@@ -3,7 +3,6 @@ const DiskStorage = require("../providers/DiskStorage");
 
 async function updateUserAvatar(req, res) {
     const userId = req.userId;
-    const avatarFilename = req.file.filename;
 
     const user = await Usuario.findById(userId);
 
@@ -12,15 +11,36 @@ async function updateUserAvatar(req, res) {
     }
 
     if (user.avatar) {
-        await DiskStorage.deleteFile(user.avatar);
+        await DiskStorage.deleteFile(`uploads/${user.avatar}`);
     }
 
-    const publicUrl = await DiskStorage.saveFile(avatarFilename);
-    user.avatar = publicUrl ;
+    const filePath = await DiskStorage.saveFile(req.file);
+    const avatarFilename = filePath.replace("uploads/", "");
+    user.avatar = avatarFilename;
 
-    await Usuario.findOneAndUpdate({ _id: userId }, { avatar: publicUrl });
+    await Usuario.findOneAndUpdate({ _id: userId }, { avatar: avatarFilename });
 
     return res.status(201).json({ msg: "Foto atualizada com sucesso", user })
 }
 
-module.exports = { updateUserAvatar };
+async function showAvatar(req, res) {
+    const { file } = req.params;
+
+    try {
+        const data = await DiskStorage.downloadFile(`uploads/${file}`);
+
+        const arrayBuffer = await data.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        res.set('Content-Type', data.type);
+
+        return res.send(buffer);
+        
+    } catch (error) {
+
+        return res.status(404).json({ msg: "Imagem não encontrada" });
+
+    }
+}
+
+module.exports = { updateUserAvatar, showAvatar };
