c45d7a10a68b7605ea0ee3eec16dcf35dce3df61
gabrielsb18 <gbmodelator@gmail.com>
2026-03-03 02:49:59 -0300

    fix: corrigir formatação de código em db-handler.js

--- tests/db-handler.js
+++ tests/db-handler.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 
 let mongoServer;
 
-module.exports.connect = async ()=> {
+module.exports.connect = async () => {
     mongoServer = await MongoMemoryServer.create();
     const uri = mongoServer.getUri();
 
     await mongoose.connect(uri);
 };
 
-module.exports.closeDatabase = async ()=> {
+module.exports.closeDatabase = async () => {
     await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
     await mongoServer.stop();
@@ -19,7 +19,7 @@ module.exports.closeDatabase = async ()=> {
 module.exports.clearDatabase = async () => {
     const collections = mongoose.connection.collections;
 
-    for (const key in collections ) {
+    for (const key in collections) {
         const collection = collections[key];
         await collection.deleteMany({});
     }
