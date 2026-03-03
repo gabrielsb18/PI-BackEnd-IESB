9ddd018cfb7dd099559ecaf085af6ce1d770612c
gabrielsb18 <gbmodelator@gmail.com>
2026-03-03 02:49:09 -0300

    feat: adicionar configuração do Jest para testes automatizados

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+    testMatch: ['**/tests/*.test.js'],
+    verbose: true,
+    forceExit: true,
+
+    clearMocks: true,
+    resetMocks: true,
+    restoreMocks: true,
+
+    testTimeout: 30000,
+}
